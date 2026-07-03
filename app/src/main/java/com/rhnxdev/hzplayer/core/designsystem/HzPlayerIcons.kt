31d0bd1c61be2c14319b7dbacb473fda38830aec
Rey Panda <reiyhan.aditya@gmail.com>
2026-07-03 12:18:15 +0700

    feat: update core components and design system icons

--- app/src/main/java/com/rhnxdev/hzplayer/core/designsystem/HzPlayerIcons.kt
+++ app/src/main/java/com/rhnxdev/hzplayer/core/designsystem/HzPlayerIcons.kt
@@ -1,9 +1,14 @@
 package com.rhnxdev.hzplayer.core.designsystem
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Dns
 import androidx.compose.material.icons.filled.Folder
+import androidx.compose.material.icons.filled.Language
 import androidx.compose.material.icons.filled.Movie
 import androidx.compose.material.icons.filled.MusicNote
+import androidx.compose.material.icons.filled.OndemandVideo
 import androidx.compose.material.icons.filled.Pause
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material.icons.filled.Repeat
@@ -12,6 +17,8 @@ import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.Shuffle
 import androidx.compose.material.icons.filled.SkipNext
 import androidx.compose.material.icons.filled.SkipPrevious
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.filled.StarOutline
 import androidx.compose.material.icons.filled.VideoLibrary
 import androidx.compose.material.icons.filled.VolumeUp
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -22,10 +29,17 @@ object HzPlayerIcons {
     val FileBrowser: ImageVector get() = Icons.Filled.Folder
     val Settings: ImageVector get() = Icons.Filled.Settings
     val Browse: ImageVector get() = Icons.Filled.Folder
+    val Network: ImageVector get() = Icons.Filled.Language
 
     val Video: ImageVector get() = Icons.Filled.Movie
     val Audio: ImageVector get() = Icons.Filled.MusicNote
     val Folder: ImageVector get() = Icons.Filled.Folder
+    val Server: ImageVector get() = Icons.Filled.Dns
+    val Stream: ImageVector get() = Icons.Filled.OndemandVideo
+    val Star: ImageVector get() = Icons.Filled.Star
+    val StarOutline: ImageVector get() = Icons.Filled.StarOutline
+    val Add: ImageVector get() = Icons.Filled.Add
+    val Delete: ImageVector get() = Icons.Filled.Delete
 
     val Play: ImageVector get() = Icons.Filled.PlayArrow
     val Pause: ImageVector get() = Icons.Filled.Pause
